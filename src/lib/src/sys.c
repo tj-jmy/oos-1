a0ca7e43017b2f3bfefdb745b91bb39fab0c8464
jmy <2250954@tongji.edu.cn>
2024-11-18 20:23:48 +0800

    加系统调用

--- src/lib/src/sys.c
+++ src/lib/src/sys.c
@@ -5,10 +5,10 @@ int execv(char *pathname, char *argv[])
 {
 	int res;
 	int argc = 0;
-	while(argv[argc] != 0)
-		argc++;	
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(11),"b"(pathname),"c"(argc),"d"(argv));
-	if ( res >= 0 )
+	while (argv[argc] != 0)
+		argc++;
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(11), "b"(pathname), "c"(argc), "d"(argv));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -16,26 +16,26 @@ int execv(char *pathname, char *argv[])
 int fork()
 {
 	int res;
-	__asm__ __volatile__ ( "int $0x80":"=a"(res):"a"(2));
-	if ( res >= 0 )
+	__asm__ __volatile__("int $0x80" : "=a"(res) : "a"(2));
+	if (res >= 0)
 		return res;
 	return -1;
 }
 
-int wait(int* status)	/* 获取子进程返回的Return Code */
+int wait(int *status) /* 获取子进程返回的Return Code */
 {
 	int res;
-	__asm__ __volatile__ ( "int $0x80":"=a"(res):"a"(7),"b"(status));
-	if ( res >= 0 )
+	__asm__ __volatile__("int $0x80" : "=a"(res) : "a"(7), "b"(status));
+	if (res >= 0)
 		return res;
 	return -1;
 }
 
-int exit(int status)	/* 子进程返回给父进程的Return Code */
+int exit(int status) /* 子进程返回给父进程的Return Code */
 {
 	int res;
-	__asm__ __volatile__ ( "int $0x80":"=a"(res):"a"(1),"b"(status));
-	if ( res >= 0 )
+	__asm__ __volatile__("int $0x80" : "=a"(res) : "a"(1), "b"(status));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -43,8 +43,8 @@ int exit(int status)	/* 子进程返回给父进程的Return Code */
 int signal(int signal, void (*func)())
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(48),"b"(signal), "c"(func) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(48), "b"(signal), "c"(func));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -52,8 +52,8 @@ int signal(int signal, void (*func)())
 int kill(int pid, int signal)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(37),"b"(pid), "c"(signal) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(37), "b"(pid), "c"(signal));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -61,31 +61,31 @@ int kill(int pid, int signal)
 int sleep(unsigned int seconds)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(35),"b"(seconds) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(35), "b"(seconds));
+	if (res >= 0)
 		return res;
 	return -1;
 }
 
 /* 使用errno需要include "stdlib.h" */
 extern errno;
-int brk(void * newEndDataAddr)
+int brk(void *newEndDataAddr)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(17),"b"(newEndDataAddr));
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(17), "b"(newEndDataAddr));
 	/* 系统调用的返回值赋值APP的全局变量errno */
-	if ( res >= 0 )
+	if (res >= 0)
 		return res;
-	errno = -1*res;
-	printf("%d\n",errno);
+	errno = -1 * res;
+	printf("%d\n", errno);
 	return -1;
 }
 
 int syncFileSystem()
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(36) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(36));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -93,8 +93,8 @@ int syncFileSystem()
 int getPath(char *path)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(39),"b"(path));
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(39), "b"(path));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -102,8 +102,8 @@ int getPath(char *path)
 int getpid()
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(20) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(20));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -111,8 +111,8 @@ int getpid()
 unsigned int getgid()
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(47) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(47));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -120,8 +120,8 @@ unsigned int getgid()
 unsigned int getuid()
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(24) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(24));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -129,8 +129,8 @@ unsigned int getuid()
 int setgid(short gid)
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(46),"b"(gid) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(46), "b"(gid));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -138,26 +138,26 @@ int setgid(short gid)
 int setuid(short uid)
 {
 	int res;
-	__asm__ volatile ( "int $0x80":"=a"(res):"a"(23),"b"(uid) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(23), "b"(uid));
+	if (res >= 0)
 		return res;
 	return -1;
 }
 
-int gettime(struct tms* ptms)
+int gettime(struct tms *ptms)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(13),"b"(ptms) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(13), "b"(ptms));
+	if (res >= 0)
 		return res;
 	return -1;
 }
 
-int times(struct tms* ptms)
+int times(struct tms *ptms)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(43),"b"(ptms) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(43), "b"(ptms));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -165,8 +165,8 @@ int times(struct tms* ptms)
 int getswtch()
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(38) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(38));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -174,8 +174,8 @@ int getswtch()
 int trace(int lines)
 {
 	int res;
-	__asm__ volatile ("int $0x80":"=a"(res):"a"(29),"b"(lines) );
-	if ( res >= 0 )
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(29), "b"(lines));
+	if (res >= 0)
 		return res;
 	return -1;
 }
@@ -191,4 +191,22 @@ int sbrk(int increment)
 	brk(((newedata >> 12) + 1) << 12);
 	fakeedata = newedata + 1;
 	return fakeedata;
+}
+
+int getpid2()
+{
+	int res;
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(50));
+	if (res >= 0)
+		return res;
+	return -1;
+}
+
+int getppid()
+{
+	int res;
+	__asm__ volatile("int $0x80" : "=a"(res) : "a"(51));
+	if (res >= 0)
+		return res;
+	return -1;
 }
--- src/program/showStack.c
+++ src/program/showStack.c
@@ -1,34 +1,10 @@
 #include <stdio.h>
-int version = 1;
+#include <sys.h>
 
 void main1()
 {
-    int a, b, result;
-    a = 1;
-    b = 2;
-    result = sum(a, b);
-
-    // while (1)
-    // {
-    //     void *ptr = malloc(1024 * 1024); // 分配 1MB 内存
-    //     printf("ptr=%x\n", ptr);
-    //     if (ptr == 0)
-    //     {
-    //         printf("内存分配失败，可能已经耗尽所有可用内存。\n");
-    //         break;
-    //     }
-    //     // 可以在这里做一些操作，但不要释放内存
-    // }
-    // return 0;
-
-    printf("result=%d\n", result);
-
-    // printf("main1 address: %x\n", main1);
-    // printf("sum address: %x\n", sum);
-    // printf("version address: %x\n", &version);
-    // printf("a address: %x\n", &a);
-    // printf("b address: %x\n", &b);
-    // printf("result address: %x\n", &result);
+    printf("getpid2: %d\n", getpid2());
+    printf("getppid: %x\n", getppid());
 }
 
 int sum(int var1, int var2) // 显式声明参数类型
